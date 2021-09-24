f6246ce9ad4c3cfd94fc4e1866569af6556e6ce9
Ash-intosh <60282134+Ash-intosh@users.noreply.github.com>
2021-09-24 19:15:24 +0530

    Update providers.tf

--- storage/providers.tf
+++ storage/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-
+  use_msi = true
   subscription_id = "d7eb1cba-f5ff-4f6d-9eca-2725372f0266"
   tenant_id = "76a2ae5a-9f00-4f6b-95ed-5d33d77c4d61"
 }
